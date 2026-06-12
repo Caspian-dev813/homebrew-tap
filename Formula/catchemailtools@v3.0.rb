18cf6a2fc9c42fe9977970562197b7a9117887f0
Caspian-dev813 <MikeHuang5286@outlook.com>
2026-06-12 18:51:57 +0800

    Add CatchEmailTools all versions

---
+++ Formula/catchemailtools@v3.0.rb
@@ -0,0 +1,10 @@
+class CatchemailtoolsV30 < Formula
+  desc "CatchEmailTools v3.0 (macOS arm64)"
+  homepage "https://github.com/Caspian-dev813/CatchEmailTools"
+  url "https://github.com/Caspian-dev813/CatchEmailTools/releases/download/v3.0/CatchEmailTools_Darwin_arm64_v3.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "MIT"
+  def install
+    bin.install "CatchEmailTools" => "catchemailtools"
+  end
+end
