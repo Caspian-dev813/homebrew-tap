97dec86044c7f3d0a136dab9f278f58f6c040eee
Caspian-dev813 <MikeHuang5286@outlook.com>
2026-06-12 21:53:06 +0800

    Update all checksums to real value

--- Formula/catchemailtools-v1-0.rb
+++ Formula/catchemailtools-v1-0.rb
@@ -2,7 +2,7 @@ class CatchemailtoolsV10 < Formula
   desc "CatchEmailTools v1.0 (GitCode Binary)"
   homepage "https://gitcode.com/MikeHuang8358/CatchEmailTools"
   url "https://gitcode.com/MikeHuang8358/CatchEmailTools/releases/download/v1.0/CatchEmailTools_Darwin_arm64_silicon_1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "06ea9dfb41a4f6e610d90052e82f64d5635033becefad39e7254e6354a949143"
   license "MIT"
   def install
     bin.install "CatchEmailTools" => "catchemailtools"
