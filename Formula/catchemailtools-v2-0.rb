44af01744516eb03c286e357d2e06380f8cd1bc5
Caspian-dev813 <MikeHuang5286@outlook.com>
2026-06-12 19:01:37 +0800

    Use source code compile, fix 404 error

--- Formula/catchemailtools-v2-0.rb
+++ Formula/catchemailtools-v2-0.rb
@@ -1,10 +1,11 @@
 class CatchemailtoolsV20 < Formula
   desc "CatchEmailTools v2.0"
   homepage "https://github.com/Caspian-dev813/CatchEmailTools"
-  url "https://github.com/Caspian-dev813/CatchEmailTools/releases/download/v2.0/CatchEmailTools_Darwin_arm64_v2.0.tar.gz"
+  url "https://github.com/Caspian-dev813/CatchEmailTools/archive/refs/tags/v2.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   def install
+    system "g++", "CatchEmailTools.cpp", "-o", "CatchEmailTools"
     bin.install "CatchEmailTools" => "catchemailtools"
   end
 end
--- Formula/catchemailtools-v4-1.rb
+++ Formula/catchemailtools-v4-1.rb
@@ -1,10 +1,11 @@
 class CatchemailtoolsV41 < Formula
   desc "CatchEmailTools v4.1"
   homepage "https://github.com/Caspian-dev813/CatchEmailTools"
-  url "https://github.com/Caspian-dev813/CatchEmailTools/releases/download/v4.1/CatchEmailTools_Darwin_arm64_v4.1.tar.gz"
+  url "https://github.com/Caspian-dev813/CatchEmailTools/archive/refs/tags/v4.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   def install
+    system "g++", "CatchEmailTools.cpp", "-o", "CatchEmailTools"
     bin.install "CatchEmailTools" => "catchemailtools"
   end
 end
